3aabe2e859664507d090c7169f9dcffb4fac2c6c
K8059 <my2722@columbia.edu>
2021-11-11 17:05:46 -0500

    public cipher

--- HW7-public/cipher_my2722/tests/test_cipher_my2722.py
+++ HW7-public/cipher_my2722/tests/test_cipher_my2722.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 from cipher_my2722 import cipher_my2722
+
+def test_cipher():
+    result=cipher(text ='hi',1,True)
+    assert result == 'ij'
+
